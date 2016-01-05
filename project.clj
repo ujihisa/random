dd2394470ce647e47797b7e3c3ba88d5f4ee58b1
ujihisa <ujihisa@gmail.com>
2016-01-05 14:57:31 -0800

    GPLv3+

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject dijkstra "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GPL3 or any later versions"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot dijkstra.core
   :target-path "target/%s"

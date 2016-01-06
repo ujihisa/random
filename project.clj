73448ae32ecb4e956871c315e32d4cc44b7de4f5
ujihisa <ujihisa@gmail.com>
2016-01-06 00:07:51 -0800

    Use latest clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL3 or any later versions"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]]
   :main ^:skip-aot dijkstra.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

4bd7f29eff5567c402dc7396add88facb6f10b9e
ujihisa <ujihisa@gmail.com>
2016-01-07 15:32:00 -0800

    solve it

--- src/dijkstra/core.clj
+++ src/dijkstra/core.clj
@@ -1,6 +1,23 @@
 (ns dijkstra.core
   (:gen-class))
 
+(defn solve
+  "Returns a route like, [(:a :b :e) 2]"
+  [all-vertices distances start-v goal-v]
+  (if (= start-v goal-v)
+    [[goal-v] 0]
+    (let [next-routes (for [v (disj all-vertices start-v)
+                            :let [result (solve (disj all-vertices start-v) distances v goal-v)]
+                            :when (not= "NO ROUTE FOUND" result)]
+                        result)
+          routes-with-me (for [[path cost] next-routes]
+                           (when-let [new-cost (distances [start-v (first path)])]
+                             [(cons start-v path) (+ new-cost cost)]))
+          routes-with-me (remove nil? routes-with-me)]
+      (if (seq routes-with-me)
+        (apply min-key second routes-with-me)
+        "NO ROUTE FOUND"))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]

31f2abb46502a90f7da36537775526c096842c34
ujihisa <ujihisa@gmail.com>
2016-01-08 15:02:50 -0800

    Use lein-expectation

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL3 or any later versions"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+                 [expectations "2.0.9"]]
+  :plugins [[lein-expectations "0.0.8"]]
   :main ^:skip-aot dijkstra.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

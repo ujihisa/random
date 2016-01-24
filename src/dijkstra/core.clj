60f8ec3b1e9b45bdcd81de1d7ff72fff6211c11c
ujihisa <ujihisa@gmail.com>
2016-01-24 15:36:42 -0800

    Indent

--- src/dijkstra/core.clj
+++ src/dijkstra/core.clj
@@ -6,14 +6,20 @@
   [all-vertices distances start-v goal-v]
   (if (= start-v goal-v)
     [[goal-v] 0]
-    (let [next-routes (for [v (disj all-vertices start-v)
-                            :let [result (solve (disj all-vertices start-v) distances v goal-v)]
-                            :when (not= "NO ROUTE FOUND" result)]
-                        result)
-          routes-with-me (for [[path cost] next-routes]
-                           (when-let [new-cost (distances [start-v (first path)])]
-                             [(cons start-v path) (+ new-cost cost)]))
-          routes-with-me (remove nil? routes-with-me)]
+    (let [next-routes
+          (for [v (disj all-vertices start-v)
+                :let [result
+                      (solve (disj all-vertices start-v) distances v goal-v)]
+                :when (not= "NO ROUTE FOUND" result)]
+            result)
+
+          routes-with-me
+          (for [[path cost] next-routes]
+            (when-let [new-cost (distances [start-v (first path)])]
+              [(cons start-v path) (+ new-cost cost)]))
+
+          routes-with-me
+          (remove nil? routes-with-me)]
       (if (empty? routes-with-me)
         "NO ROUTE FOUND"
         (apply min-key second routes-with-me)))))

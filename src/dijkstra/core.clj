0ddf83d598efd65fa7f74ee46fc1d74c9b4f6f06
ujihisa <ujihisa@gmail.com>
2016-01-24 15:36:04 -0800

    Refactor

--- src/dijkstra/core.clj
+++ src/dijkstra/core.clj
@@ -2,7 +2,7 @@
   (:gen-class))
 
 (defn solve
-  "Returns a route like, [(:a :b :e) 2]"
+  "Returns a route like, [(:a :b :e) 2], or \"NO ROUTE_ROUND\""
   [all-vertices distances start-v goal-v]
   (if (= start-v goal-v)
     [[goal-v] 0]
@@ -14,9 +14,9 @@
                            (when-let [new-cost (distances [start-v (first path)])]
                              [(cons start-v path) (+ new-cost cost)]))
           routes-with-me (remove nil? routes-with-me)]
-      (if (seq routes-with-me)
-        (apply min-key second routes-with-me)
-        "NO ROUTE FOUND"))))
+      (if (empty? routes-with-me)
+        "NO ROUTE FOUND"
+        (apply min-key second routes-with-me)))))
 
 (defn -main
   "I don't do a whole lot ... yet."

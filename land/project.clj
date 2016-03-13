05ea0a26e416c212e17a366c2c0b837cf2d27f3f
ujihisa <ujihisa@gmail.com>
2016-03-12 23:19:56 -0800

    clojure 1.8

--- land/project.clj
+++ land/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL-3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot land.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

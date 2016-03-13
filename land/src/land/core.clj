4abde0a06bd4be54b26c103c81102d8cc91dbb73
ujihisa <ujihisa@gmail.com>
2016-03-12 23:34:33 -0800

    examples with mock

--- land/src/land/core.clj
+++ land/src/land/core.clj
@@ -1,6 +1,27 @@
 (ns land.core
   (:gen-class))
 
+(defn flat?'
+  "true if all the heights of the top blocks of the chunk are same"
+  [chunk getHighestBlockAt getWorld]
+  (let [blocks (for [x (range 16)
+                     z (range 16)]
+                 (getHighestBlockAt (getWorld chunk) x z))]
+    (first blocks)))
+
+(defn flat? [chunk]
+  (flat?' chunk
+          (fn [world x y z]
+            (.getHighestBlockAt world x y z))
+          (fn [chunk]
+            (.getWorld chunk))))
+
+(prn (let [getHighestBlockAt
+           (fn [world xdiff zdiff] :dirt)
+           getWorld
+           (fn [chunk] :world)]
+       (flat?' :dummy-chunk getHighestBlockAt getWorld)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]

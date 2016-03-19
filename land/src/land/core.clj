49b8c896ad4a92ef4437ebc0a7b7aa4d935b2a74
ujihisa <ujihisa@gmail.com>
2016-03-19 12:09:02 -0700

    Add gen-smooth-chunk
    * Thanks @aomoriringo!

--- land/src/land/core.clj
+++ land/src/land/core.clj
@@ -12,12 +12,66 @@
                  (getHighestBlockAt (getChunkSnapshot chunk) x z))]
     (first blocks)))
 
-(defn flat? [chunk]
-  (flat?' chunk
+(defn smooth? [chunk]
+  (smooth?' chunk
           (fn [chunk-snapshot x y z]
             (.getHighestBlockAt chunk-snapshot x z))
           (fn [chunk]
             (.getChunkSnapshot chunk))))
+
+;---
+(defn debug-print-chunk [chunk]
+  (println "-----------------------------------------------")
+  (doseq [x (range 16)]
+    (doseq [z (range 16)]
+      (print (get chunk [x z])
+             ""))
+    (prn))
+  (println "-----------------------------------------------"))
+
+(defn gen-rand-chunk []
+  (->>
+    (for [x (range 16)
+          z (range 16)]
+      [[x z] (inc (rand-int 10))])
+    (into {})))
+
+(defn gen-flat-chunk []
+  (let [y (inc (rand-int 10))]
+    (->>
+      (for [x (range 16)
+            z (range 16)]
+        [[x z] y])
+      (into {}))))
+
+(defn gen-smooth-chunk []
+  (letfn [(gen [constraint-points]
+            (if (seq constraint-points)
+              (let [[base & constraint-points] constraint-points]
+                (rand-nth
+                  (for [diff [-1 0 1]
+                        :let [y (+ base diff)]
+                        :when (every? #(<= -1 (- y %) 1) constraint-points)]
+                    y)))
+              (gen [(inc (rand-int 10))])))]
+    (loop [i 0 memo {}]
+      (if (<= 16 i)
+        memo
+        (recur (inc i)
+               (loop [j 0 memo memo]
+                 (if (<= 16 j)
+                   memo
+                   (let [constraint-points
+                         (for [xdiff [-1 0 1]
+                               zdiff [-1 0 1]
+                               :when (not= 0 xdiff zdiff)
+                               :let [y (get memo [(- i xdiff) (- j zdiff)])]
+                               :when y]
+                           y)]
+                     (recur (inc j)
+                            (conj memo [[i j] (gen constraint-points)]))))))))))
+
+(debug-print-chunk (gen-smooth-chunk))
 
 (let [dummy-chunk-snapshot
       (->> (for [x (range 16)
@@ -28,7 +82,7 @@
       (fn [chunk-snapshot x z] :dirt)
       getChunkSnapshot
       (fn [chunk] :world)]
-  (debug-print-chunk dummy-chunk-snapshot)
+  #_ (debug-print-chunk dummy-chunk-snapshot)
   (prn (smooth?' :dummy-chunk getHighestBlockAt getChunkSnapshot)))
 
 ; land-purchasable == smooth? + no-water?)
